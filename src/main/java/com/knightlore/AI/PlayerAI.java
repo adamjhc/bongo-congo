71355f6e044784e810f1884371fec54cadb86fcf
jkh703 <jkh703@student.bham.ac.uk>
2019-01-23 13:40:29 +0000

    Wrote out a basic plan for AI player

--- src/main/java/com/knightlore/AI/PlayerAI.java
+++ src/main/java/com/knightlore/AI/PlayerAI.java
@@ -1,4 +1,20 @@
 package com.knightlore.AI;
 
+/*
+The AI that controls the computer player character when doing single player.
+    - A* pathfinding?
+    - map: 3D array
+    - consider "blocked off" nodes - 'get status' or is it something else
+
+Current approach:
+    - Get start and end position
+    - Get map data
+    - A* search path
+    - Create queue of actions
+    - Execute actions
+    - ???
+    - Win
+*/
 public class PlayerAI {
+
 }

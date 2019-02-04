b44a136714de87e50d1eafd47b860a8fe242fd74
Adam Cox <ajc785@cs.bham.ac.uk>
2019-02-04 13:26:12 +0000

    Added methods to ShaderProgram for setting uniforms

--- src/main/java/com/knightlore/client/render/ShaderProgram.java
+++ src/main/java/com/knightlore/client/render/ShaderProgram.java
@@ -8,12 +8,22 @@ import static org.lwjgl.opengl.GL20.glAttachShader;
 import static org.lwjgl.opengl.GL20.glCreateProgram;
 import static org.lwjgl.opengl.GL20.glGetProgramInfoLog;
 import static org.lwjgl.opengl.GL20.glGetProgrami;
+import static org.lwjgl.opengl.GL20.glGetUniformLocation;
 import static org.lwjgl.opengl.GL20.glLinkProgram;
+import static org.lwjgl.opengl.GL20.glUniform1i;
+import static org.lwjgl.opengl.GL20.glUniform3fv;
+import static org.lwjgl.opengl.GL20.glUniform4fv;
 import static org.lwjgl.opengl.GL20.glValidateProgram;
+
+import com.knightlore.client.render.math.Vector3f;
+import com.knightlore.client.render.math.Vector4f;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShaderProgram {
 
   private final int id;
+  private Map<String, Integer> locationCache = new HashMap<>();
 
   public ShaderProgram(String vertexShaderPath, String fragmentShaderPath) {
     id = glCreateProgram();
@@ -33,6 +43,7 @@ public class ShaderProgram {
 
   /**
    * Returns the assigned OpenGL Id of the shader program
+   *
    * @return Shader program Id
    */
   public int getId() {
@@ -53,5 +64,31 @@ public class ShaderProgram {
 
   public void validate() {
     glValidateProgram(id);
+  }
+
+  public int getUniformLocation(String name) {
+    if (locationCache.containsKey(name)) {
+      return locationCache.get(name);
+    }
+
+    int location = glGetUniformLocation(id, name);
+    if (location == -1) {
+      throw new RuntimeException(glGetProgramInfoLog(id));
+    }
+
+    locationCache.put(name, location);
+    return location;
+  }
+
+  public void setUniform(String name, int value) {
+    glUniform1i(getUniformLocation(name), value);
+  }
+
+  public void setUniform(String name, Vector3f value) {
+    glUniform3fv(getUniformLocation(name), value.toBuffer());
+  }
+
+  public void setUniform(String name, Vector4f value) {
+    glUniform4fv(getUniformLocation(name), value.toBuffer());
   }
 }

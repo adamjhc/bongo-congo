a9498cab558eaf290e556b94d2193421930c34d6
Adam Cox <ajc785@cs.bham.ac.uk>
2019-02-04 18:49:49 +0000

    Started on test classes for Vectors

---
+++ src/test/java/com/knightlore/game/math/Vector3fTest.java
@@ -0,0 +1,18 @@
+package com.knightlore.game.math;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class Vector3fTest {
+
+  @Test
+  public void canCreateVector() {
+    Vector3f vector = new Vector3f(1f, 2f, 3f);
+
+    assertThat(1f, equalTo(vector.x));
+    assertThat(2f, equalTo(vector.y));
+    assertThat(3f, equalTo(vector.z));
+  }
+}

22f05fe6c1cbc54772a8e3b77403e23cb6788bd0
Adam Cox <ajc785@cs.bham.ac.uk>
2019-02-05 14:06:54 +0000

    Made render method not abstract since most objects will have the same render instructions
    This can simply be overridden if an object requires custom render code

--- src/main/java/com/knightlore/client/render/Renderable.java
+++ src/main/java/com/knightlore/client/render/Renderable.java
@@ -1,6 +1,7 @@
 package com.knightlore.client.render;
 
 import com.knightlore.game.math.Matrix4f;
+import com.knightlore.game.math.Vector3f;
 
 public abstract class Renderable {
 
@@ -8,5 +9,18 @@ public abstract class Renderable {
   protected ShaderProgram shaderProgram;
   protected Texture texture;
 
-  public abstract void render(int x, int y, Matrix4f world, Camera camera);
+  public void render(int x, int y, Matrix4f world, Camera camera) {
+    shaderProgram.bind();
+    texture.bind(0);
+
+    Matrix4f position = new Matrix4f().translate(new Vector3f(x, y, 0));
+    Matrix4f target = new Matrix4f();
+    camera.getProjection().mul(world, target);
+    target.mul(position);
+
+    shaderProgram.setUniform("sampler", 0);
+    shaderProgram.setUniform("projection", target);
+
+    model.render();
+  }
 }

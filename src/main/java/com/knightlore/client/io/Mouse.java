1bc1b9ef854eecceff9127939167bcbdc140b7bf
Adam Cox <ajc785@cs.bham.ac.uk>
2019-02-05 12:46:41 +0000

    Update Mouse to be a GLFW callback
    Not sure why I didn't do this in the first place but there was probably a reason...

--- src/main/java/com/knightlore/client/io/Mouse.java
+++ src/main/java/com/knightlore/client/io/Mouse.java
@@ -2,16 +2,18 @@ package com.knightlore.client.io;
 
 import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_LAST;
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.glfwGetCursorPos;
 import static org.lwjgl.glfw.GLFW.glfwGetMouseButton;
 
-public class Mouse {
+import java.nio.DoubleBuffer;
+import org.joml.Vector2i;
+import org.lwjgl.glfw.GLFWMouseButtonCallback;
+import org.lwjgl.system.MemoryStack;
+
+public class Mouse extends GLFWMouseButtonCallback {
 
   private static long window = 0;
   private static boolean[] previousMouseButtonStates = new boolean[GLFW_MOUSE_BUTTON_LAST];
-
-  public static void setWindow(long windowNew) {
-    window = windowNew;
-  }
 
   public static boolean isMouseButtonDown(int button) {
     return glfwGetMouseButton(window, button) == GLFW_PRESS;
@@ -25,9 +27,31 @@ public class Mouse {
     return !isMouseButtonDown(button) && previousMouseButtonStates[button];
   }
 
-  public static void update() {
-    for (int i = 0; i < previousMouseButtonStates.length; i++) {
-      previousMouseButtonStates[i] = isMouseButtonDown(i);
+  public static boolean isMouseButtonReleasedInArea(
+      int button, Vector2i bottomLeft, Vector2i topRight) {
+    if (!isMouseButtonReleased(button)) {
+      return false;
     }
+
+    double x, y;
+    try (MemoryStack stack = MemoryStack.stackPush()) {
+      DoubleBuffer xBuffer = stack.mallocDouble(1);
+      DoubleBuffer yBuffer = stack.mallocDouble(1);
+      glfwGetCursorPos(window, xBuffer, yBuffer);
+
+      x = xBuffer.get();
+      y = yBuffer.get();
+    }
+
+    return bottomLeft.x <= x && x <= topRight.x && bottomLeft.y <= y && y <= topRight.y;
+  }
+
+  @Override
+  public void invoke(long window, int button, int action, int mods) {
+    if (window == 0) {
+      Mouse.window = window;
+    }
+
+    previousMouseButtonStates[button] = action == GLFW_PRESS;
   }
 }

ed59517a30f30438a1b90d5423a6376783da66e3
Adam Cox <ajc785@cs.bham.ac.uk>
2019-02-06 11:56:56 +0000

    Wrapped setOrtho2D for camera purposes

--- src/main/java/com/knightlore/game/math/Matrix4f.java
+++ src/main/java/com/knightlore/game/math/Matrix4f.java
@@ -9,7 +9,7 @@ public class Matrix4f extends org.joml.Matrix4f {
     super();
   }
 
-  public Matrix4f(org.joml.Matrix4f matrix4f) {
+  private Matrix4f(org.joml.Matrix4f matrix4f) {
     super(matrix4f);
   }
 
@@ -24,5 +24,9 @@ public class Matrix4f extends org.joml.Matrix4f {
 
   public Matrix4f setTranslation(Vector3f vector3f) {
     return new Matrix4f(super.setTranslation(vector3f));
+  }
+
+  public Matrix4f setOrtho2D(float left, float right, float bottom, float top) {
+    return new Matrix4f(super.setOrtho2D(left, right, bottom, top));
   }
 }

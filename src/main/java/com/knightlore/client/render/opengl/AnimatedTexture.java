e48eb03f7b0ee8c074710ed4ec48029e29671063
Adam Cox <ajc785@cs.bham.ac.uk>
2019-02-10 19:12:31 +0000

    Comments and cleanup

--- src/main/java/com/knightlore/client/render/opengl/AnimatedTexture.java
+++ src/main/java/com/knightlore/client/render/opengl/AnimatedTexture.java
@@ -3,53 +3,81 @@ package com.knightlore.client.render.opengl;
 import static org.lwjgl.glfw.GLFW.glfwGetTime;
 
 public class AnimatedTexture extends Texture {
-  private StaticTexture[] frames;
-  private int textureIndex;
 
+  /** Frames of the animation */
+  private StaticTexture[] frames;
+
+  /** The index of the current texture to render */
+  private int currentTextureIndex;
+
+  /** Amount of time passed since the texture was changed last */
   private double delta;
-  private double currentTime;
+
+  /** The last system time when the texture was updated */
   private double previousTime;
+
+  /** The target amount of time between each frame update */
   private double interval;
 
+  /**
+   * Initialise the Animated texture
+   *
+   * @param textureFileName Name of the texture prefix (with _X)
+   * @param noOfFrames Number of frames in the animation
+   * @param fps Number of frames to render every second
+   */
   public AnimatedTexture(String textureFileName, int noOfFrames, int fps) {
     frames = new StaticTexture[noOfFrames];
     for (int i = 0; i < noOfFrames; i++) {
       frames[i] = new StaticTexture(textureFileName + "_" + i);
     }
 
-    textureIndex = 0;
+    currentTextureIndex = 0;
     delta = 0;
-    currentTime = 0;
     previousTime = glfwGetTime();
     interval = 1.0 / fps;
   }
 
+  /**
+   * Get the width of the first texture in pixels
+   *
+   * @return Width of the first texture in pixels
+   */
   @Override
   public int getWidth() {
     return frames[0].getWidth();
   }
 
+  /**
+   * Get the height of the first texture in pixels
+   *
+   * @return Height of the first texture in pixels
+   */
   @Override
   public int getHeight() {
     return frames[0].getHeight();
   }
 
+  /**
+   * Calculate the current frame to bind and binds it
+   *
+   * @param sampler Sampler of the texture
+   */
   @Override
   public void bind(int sampler) {
-    this.currentTime = glfwGetTime();
-    this.delta += currentTime - previousTime;
+    double currentTime = glfwGetTime();
+    delta += currentTime - previousTime;
+    previousTime = currentTime;
 
     if (delta >= interval) {
       delta = 0;
-      textureIndex++;
+      currentTextureIndex++;
     }
 
-    if (textureIndex >= frames.length) {
-      textureIndex = 0;
+    if (currentTextureIndex == frames.length) {
+      currentTextureIndex = 0;
     }
 
-    previousTime = currentTime;
-
-    frames[textureIndex].bind(sampler);
+    frames[currentTextureIndex].bind(sampler);
   }
 }

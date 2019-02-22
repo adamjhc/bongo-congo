75227e4d27851eaad6fc4645662e500af9d7661d
Adam Cox <ajc785@cs.bham.ac.uk>
2019-02-22 20:39:55 +0000

    Specific tiles are no longer classes
    It was good Tile needed to be made non-abstract otherwise this change would have never happened

--- src/main/java/com/knightlore/game/map/TileSet.java
+++ src/main/java/com/knightlore/game/map/TileSet.java
@@ -4,59 +4,26 @@ import java.util.ArrayList;
 
 public class TileSet {
 
+  private static Tile airTile = new Tile(true, false, false, false);
+  private static Tile floorTile = new Tile(true, false, false, false);
+  private static Tile wallTile = new Tile(false, false, false, false);
+  private static Tile climbableWallTile = new Tile(false, true, false, false);
+  private static Tile hazardTile = new Tile(true, false, true, false);
+  private static Tile goalTile = new Tile(true, false, false, true);
+
   private ArrayList<Tile> tileSet;
 
   public TileSet() {
     tileSet = new ArrayList<>();
-    loadTiles();
+    tileSet.add(0, airTile);
+    tileSet.add(1, floorTile);
+    tileSet.add(2, wallTile);
+    tileSet.add(3, climbableWallTile);
+    tileSet.add(4, hazardTile);
+    tileSet.add(5, goalTile);
   }
 
-  public Tile getTile(int index) {
+  Tile getTile(int index) {
     return tileSet.get(index);
-  }
-
-  private void loadTiles() {
-    tileSet.add(0, new AirTile());
-    tileSet.add(1, new FloorTile());
-    tileSet.add(2, new WallTile());
-    tileSet.add(3, new ClimbableWallTile());
-    tileSet.add(4, new HazardTile());
-    tileSet.add(5, new GoalTile());
-  }
-
-  class AirTile extends Tile {
-    AirTile() {
-      super(true, false, false, false);
-    }
-  }
-
-  class FloorTile extends Tile {
-    FloorTile() {
-      super(true, false, false, false);
-    }
-  }
-
-  class WallTile extends Tile {
-    WallTile() {
-      super(false, false, false, false);
-    }
-  }
-
-  class ClimbableWallTile extends Tile {
-    ClimbableWallTile() {
-      super(false, true, false, false);
-    }
-  }
-
-  class HazardTile extends Tile {
-    HazardTile() {
-      super(true, false, true, false);
-    }
-  }
-
-  class GoalTile extends Tile {
-    GoalTile() {
-      super(true, false, false, true);
-    }
   }
 }

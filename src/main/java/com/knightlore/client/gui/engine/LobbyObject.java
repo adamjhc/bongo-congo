49e10662a811c9d3aa76a930b1beca063bba9f57
JCT777 <JCT777@student.bham.ac.uk>
2019-03-05 13:38:34 +0000

    Added class for lobby

---
+++ src/main/java/com/knightlore/client/gui/engine/LobbyObject.java
@@ -0,0 +1,12 @@
+package com.knightlore.client.gui.engine;
+
+import com.knightlore.client.gui.engine.graphics.FontTexture;
+
+public class LobbyObject extends TextObject {
+
+	public LobbyObject(String text, FontTexture fontTexture) {
+		super(text, fontTexture);
+		
+	}
+
+}

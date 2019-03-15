88e59d634bc1053f172778c60c978965b4892f66
Adam Cox <ajc785@cs.bham.ac.uk>
2019-03-15 12:24:31 +0000

    Cleanup

--- src/main/java/com/knightlore/client/io/Mouse.java
+++ src/main/java/com/knightlore/client/io/Mouse.java
@@ -1,16 +1,16 @@
 package com.knightlore.client.io;
 
-import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_1;
-import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_2;
-import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 import static org.lwjgl.glfw.GLFW.GLFW_CURSOR;
 import static org.lwjgl.glfw.GLFW.GLFW_CURSOR_DISABLED;
 import static org.lwjgl.glfw.GLFW.GLFW_CURSOR_NORMAL;
+import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_1;
+import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_2;
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 import static org.lwjgl.glfw.GLFW.glfwSetCursorEnterCallback;
 import static org.lwjgl.glfw.GLFW.glfwSetCursorPosCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetInputMode;
 import static org.lwjgl.glfw.GLFW.glfwSetMouseButtonCallback;
 import static org.lwjgl.glfw.GLFW.glfwSetScrollCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetInputMode;
 
 public class Mouse {
 
@@ -30,21 +30,21 @@ public class Mouse {
   public static void init() {
     glfwSetCursorPosCallback(
         Window.getWindowHandle(),
-        (WindowHandle, xPosNew, yPosNew) -> {
+        (windowHandle, xPosNew, yPosNew) -> {
           xPos = xPosNew;
           yPos = yPosNew;
         });
 
     glfwSetMouseButtonCallback(
         Window.getWindowHandle(),
-        (WindowHandle, button, action, mode) -> {
+        (windowHandle, button, action, mode) -> {
           leftButtonPressed = button == GLFW_MOUSE_BUTTON_1 && action == GLFW_PRESS;
           rightButtonPressed = button == GLFW_MOUSE_BUTTON_2 && action == GLFW_PRESS;
         });
 
     glfwSetScrollCallback(
         Window.getWindowHandle(),
-        (WindowHandle, xOffset, yOffset) -> {
+        (windowHandle, xOffset, yOffset) -> {
           scrollUp = yOffset > 0;
           scrollDown = yOffset < 0;
         });
@@ -55,13 +55,13 @@ public class Mouse {
           isInScreen = entered;
         });
   }
-  
+
   public static void hideCursor() {
-  	glfwSetInputMode(Window.getWindowHandle(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(Window.getWindowHandle(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
   }
-  
+
   public static void showCursor() {
-  	glfwSetInputMode(Window.getWindowHandle(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    glfwSetInputMode(Window.getWindowHandle(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
   }
 
   public static double getXPos() {

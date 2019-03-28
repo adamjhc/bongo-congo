c98d2932237b1da669b27f3dea6056844d67b099
lewis <me@lewisrelph.com>
2019-03-28 18:44:24 +0000

    Util refactoring & commenting

--- src/main/java/com/knightlore/util/Config.java
+++ src/main/java/com/knightlore/util/Config.java
@@ -2,39 +2,39 @@ package com.knightlore.util;
 
 import java.util.Optional;
 
+/** Global configuration reader Acts as a facade for ConfigReader.java */
 public class Config {
-    private static ConfigReader env = ConfigReader.handler;
+  /** Static instance */
+  private static ConfigReader env = ConfigReader.handler;
 
-    public static Optional<String> sessionKey() {
-        Optional<String> sessionKey = env.getVariable("session_key");
+  public static Optional<String> sessionKey() {
+    Optional<String> sessionKey = env.getVariable("session_key");
 
-        // Split on comma
-        return sessionKey;
+    // Split on comma
+    return sessionKey;
+  }
+
+  public static Optional<String> apiKey() {
+    Optional<String> apiKey = env.getVariable("api_key");
+
+    // Split on comma
+    return apiKey;
+  }
+
+  public static Optional<String> authServerIp() {
+    Optional<String> authServerIp = env.getVariable("auth_server_ip");
+
+    // Split on comma
+    return authServerIp;
+  }
+
+  public static Optional<Integer> authServerPort() {
+    Optional<String> string = env.getVariable("auth_server_port");
+
+    if (string.isPresent()) {
+      return Optional.of(Integer.valueOf(string.get()));
+    } else {
+      return Optional.empty();
     }
-
-    public static Optional<String> apiKey() {
-        Optional<String> apiKey = env.getVariable("api_key");
-
-        // Split on comma
-        return apiKey;
-    }
-
-    public static Optional<String> authServerIp() {
-        Optional<String> authServerIp = env.getVariable("auth_server_ip");
-
-        // Split on comma
-        return authServerIp;
-    }
-
-    public static Optional<Integer> authServerPort() {
-        Optional<String> string =  env.getVariable("auth_server_port");
-
-        if(string.isPresent()){
-            return Optional.of(Integer.valueOf(string.get()));
-        }else{
-            return Optional.empty();
-        }
-    }
-
-
+  }
 }
--- src/main/java/com/knightlore/util/ConfigReader.java
+++ src/main/java/com/knightlore/util/ConfigReader.java
@@ -1,63 +1,72 @@
 package com.knightlore.util;
 
+import org.apache.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Optional;
 
+/**
+ * Actual implementation of config reader, all logic stored here Allows client to read fron a
+ * system.env file
+ */
 public class ConfigReader {
 
-    public static ConfigReader handler = new ConfigReader();
+  // Static reader instance (prevent re-read of file)
+  public static ConfigReader handler = new ConfigReader();
 
-    private final String ENV_PATH="system.env";
-    private HashMap<String, String> variables = new HashMap<>();
+  // File path for configuration
+  private final String ENV_PATH = "system.env";
 
-    private ConfigReader() {
-        // Read file on construction
-        this.readFile();
+  // Stored variables
+  private HashMap<String, String> variables = new HashMap<>();
 
+  private static final Logger logger = Logger.getLogger(ConfigReader.class);
+
+  /** Default constructor Read file on creation */
+  private ConfigReader() {
+    this.readFile();
+  }
+
+  /** Read environment file and update local variables to match */
+  private void readFile() {
+    String line;
+    try (BufferedReader br = new BufferedReader(new FileReader(ENV_PATH))) {
+      while ((line = br.readLine()) != null) {
+        if (!line.equals("")) addLineToVariables(line);
+      }
+
+    } catch (IOException e) {
+      logger.warn("Error duirng configuration file reading, does system.env exist?");
+    }
+  }
+
+  /**
+   * Split the provided line into variable, value pairs and assign to local variables
+   *
+   * @param line
+   */
+  private void addLineToVariables(String line) {
+    String[] split = line.split("=");
+
+    // Add to hashmap, name as key
+    this.variables.put(split[0], split[1]);
+  }
+
+  /**
+   * Retrieve raw variable value from variable list
+   *
+   * @param key
+   * @return optional, value if exists as string
+   */
+  public Optional<String> getVariable(String key) {
+    if (this.variables.containsKey(key)) {
+      return Optional.of(this.variables.get(key));
     }
 
-    /**
-     * Read environment file and update local variables to match
-     */
-    private void readFile() {
-        String line;
-        try (BufferedReader br = new BufferedReader(new FileReader(ENV_PATH))) {
-            while ((line = br.readLine()) != null) {
-
-                if(!line.equals(""))
-                    addLineToVariables(line);
-            }
-
-        }catch(IOException e){
-            System.out.println("FAIL");
-        }
-    }
-
-    /**
-     * Split the provided line into variable, value pairs and assign to local variables
-     * @param line
-     */
-    private void addLineToVariables(String line) {
-        String[] split = line.split("=");
-
-        // Append to variables
-        this.variables.put(split[0], split[1]);
-    }
-
-    /**
-     * Retrieve raw variable value from variable list
-     * @param key
-     * @return
-     */
-    public Optional<String> getVariable(String key) {
-        if(this.variables.containsKey(key)) {
-            return Optional.of(this.variables.get(key));
-        }
-
-        // Key does not exist
-        return Optional.empty();
-    }
+    // Key does not exist
+    return Optional.empty();
+  }
 }
